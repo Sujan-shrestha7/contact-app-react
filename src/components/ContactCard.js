aa208827d73e088fe36913374cb2438a345985dd
Sujanshrestha <69748127+SujanShresthaa@users.noreply.github.com>
2024-10-18 00:21:49 +0545

    Add files via upload

---
+++ src/components/ContactCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import user from "../images/user.png";
+
+const ContactCard = (props) => {
+
+    const{id, name, email}=props.contact;
+    return(
+        <div className='item'>
+            <img className="ui avatar image" src={user} alt="user" />
+        <div className='content'>
+        <div className='header'>{id}</div>
+            <div>{name}</div>
+            <div> {email}
+                
+        <i className='trash alternate outline icon' style={ {color:"red", marginTop:"7px"}}></i></div>     
+        </div>
+    </div>
+    );
+}
+
+export default ContactCard;
